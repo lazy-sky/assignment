f1aadd98c76b1bf7bd96aa2665be240e5a5dcd64
lazy-sky <1cielo@naver.com>
2022-10-26 16:50:36 +0900

    Chore: Change Header text

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,9 @@
 import style from './header.module.scss'
 
-// TODO: 받은 조직 및 저장소 이름 보여주도록
 const Header = () => {
   return (
     <header className={style.gnb}>
-      <h1>Organization Name / Repository Name</h1>
+      <h1>Angular / Angular-cli</h1>
     </header>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './Home'
 import Detail from './Detail'
+import Header from 'components/Header'
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/detail' element={<Detail />}>
-          <Route path=':id' />
+        <Route path='/issue'>
+          <Route path=':issueNumber' element={<Detail />} />
         </Route>
       </Routes>
     </div>
