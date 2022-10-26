8bf0eba0b6a5e7c0ceca3c7ba1e9f577327a2a2b
lazy-sky <1cielo@naver.com>
2022-10-26 15:18:03 +0900

    Feat: Make IssueCard

---
+++ src/components/IssueCard/index.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import { IIssue } from 'types/issue'
+
+import style from './issueCard.module.scss'
+
+interface IProps {
+  issue: IIssue
+}
+
+const IssueCard = ({ issue }: IProps) => {
+  return (
+    <li className={style.issueCard}>
+      <Link to={`/${issue.number}`}>
+        <div className={style.top}>
+          <div className={style.left}>
+            <div>#{issue.number}</div>
+            <div>{issue.title}</div>
+          </div>
+          <div className={style.comments}>코멘트: {issue.comments}</div>
+        </div>
+        <div className={style.bottom}>
+          <div>작성자: {issue.user.login},</div>
+          <div>작성일: {issue.created_at}</div>
+        </div>
+      </Link>
+    </li>
+  )
+}
+
+export default IssueCard
