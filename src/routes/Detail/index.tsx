7545d1eba5520f9817cead9ca02e71f0b3f9261f
lazy-sky <1cielo@naver.com>
2022-10-26 16:14:38 +0900

    Feat: Render back button in Detail

--- src/routes/Detail/index.tsx
+++ src/routes/Detail/index.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 import { IIssue } from 'types/issue'
 import { getIssueByNumber } from 'services/data'
@@ -8,12 +8,21 @@ import IssueCard from 'components/IssueCard'
 import style from './detail.module.scss'
 
 const Detail = () => {
+  const navigate = useNavigate()
   const params = useParams()
   const { issueNumber } = params
 
-  const { isError, isLoading, data } = useQuery<IIssue>(['issues'], () => getIssueByNumber(Number(issueNumber)), {
-    retry: 1,
-  })
+  const { isError, isLoading, data } = useQuery<IIssue>(
+    ['issue', { number: issueNumber }],
+    () => getIssueByNumber(Number(issueNumber)),
+    {
+      retry: 1,
+    }
+  )
+
+  const handleBackClick = () => {
+    navigate(-1)
+  }
 
   if (isLoading) {
     return <div>Loading...</div>
@@ -25,6 +34,9 @@ const Detail = () => {
 
   return (
     <div className={style.detail}>
+      <button type='button' onClick={handleBackClick} className={style.backBtn}>
+        뒤로 가기
+      </button>
       <div className={style.top}>
         <img src={data.user.avatar_url} alt='user profile' />
         <IssueCard issue={data} />
